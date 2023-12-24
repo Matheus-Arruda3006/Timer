085d3630b6da456c1f71715216e05a1b6f907dcd
Matheus-Arruda3006 <matheusarrudaf@gmail.com>
2023-12-24 00:32:43 -0300

    history page

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,5 +1,46 @@
+import { HistoryContainer, HistoryList } from "./stylesHistory";
+
 export function History (){
     return(
-        <h1>History</h1>
+        <HistoryContainer>
+
+        <h1>
+           My history
+        </h1>
+
+        <HistoryList>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Tarefa</th>
+                        <th>Duração</th>
+                        <th>Início</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Tarefa</td>
+                        <td>20 minutos</td>
+                        <td>Há 1 semana</td>
+                        <td>Concluido</td>
+                    </tr>
+                    <tr>
+                        <td>Tarefa</td>
+                        <td>20 minutos</td>
+                        <td>Há 1 semana</td>
+                        <td>Concluido</td>
+                    </tr>
+                    <tr>
+                        <td>Tarefa</td>
+                        <td>20 minutos</td>
+                        <td>Há 1 semana</td>
+                        <td>Concluido</td>
+                    </tr>
+                </tbody>
+            </table>
+        </HistoryList>
+
+        </HistoryContainer>
     );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,10 +7,14 @@ export function Home (){
             <form action="">
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em:</label>
-                    <TaskInput id="task" placeholder="De um nome para o seu projeto..." />
+                    <TaskInput id="task" list="task-suggestions" placeholder="De um nome para o seu projeto..." />
+                    <datalist id="task-suggestions">
+                        <option value="projeto 1"></option>
+                        <option value="projeto 2"></option>
+                    </datalist>
 
                     <label htmlFor="minutesAmount">durante</label>
-                    <MinutesAmountInput type="number" id="minutesAmount" placeholder="00"/>
+                    <MinutesAmountInput type="number" id="minutesAmount" placeholder="00" step={5} min={5} max={60}/>
 
                     <span>minutos.</span>
                 </FormContainer>
